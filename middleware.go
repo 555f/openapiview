d44203bae18e05b8c79db8c5402da342f606c269
555f <code@555f.com>
2023-09-14 12:51:12 +0300

    feat: middleware implement ServeHTTP

--- middleware.go
+++ middleware.go
@@ -12,25 +12,29 @@ type Middleware struct {
 	openapiData []byte
 }
 
+func (m *Middleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := strings.TrimPrefix(r.URL.Path, m.pathPrefix)
+	path = "static" + path
+
+	if path == "static/openapi" {
+		_, _ = w.Write(m.openapiData)
+		return
+	}
+
+	data, err := fs.ReadFile(path)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	contentType := mime.TypeByExtension(filepath.Ext(r.URL.Path))
+	w.Header().Set("Content-Type", contentType)
+	_, _ = w.Write(data)
+}
+
 func (m *Middleware) Process(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if strings.HasPrefix(r.URL.Path, m.pathPrefix) {
-			path := strings.TrimPrefix(r.URL.Path, m.pathPrefix)
-			path = "static" + path
-
-			if path == "static/openapi" {
-				_, _ = w.Write(m.openapiData)
-				return
-			}
-
-			data, err := fs.ReadFile(path)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			contentType := mime.TypeByExtension(filepath.Ext(r.URL.Path))
-			w.Header().Set("Content-Type", contentType)
-			_, _ = w.Write(data)
+			m.ServeHTTP(w, r)
 			return
 		}
 		next.ServeHTTP(w, r)
